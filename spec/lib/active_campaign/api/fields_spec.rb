a183d147e31b47818ef6daa21404b3abe2fc122a
Tim Chaston <timchaston@gmail.com>
2020-05-29 11:15:04 +1000

    Change update field values names so that they don't override their counterparts up in the antecedent contexts.

--- spec/lib/active_campaign/api/fields_spec.rb
+++ spec/lib/active_campaign/api/fields_spec.rb
@@ -36,33 +36,33 @@ RSpec.describe ActiveCampaign::API::Fields, :vcr do
   describe '#update_field', :with_existing_text_field do
     subject(:response) { client.update_field(field_id, update_params) }
 
-    let(:field_title)            { 'Custom text - updated' }
-    let(:field_type)             { 'text' }
-    let(:field_description)      { 'A custom text field - updated' }
-    let(:field_is_required)      { '1' }
-    let(:field_personalized_tag) { 'perstag' }
-    let(:field_default_value)    { 'Default value - updated' }
-    let(:field_visible)          { '1' }
-    let(:field_ordering_number)  { '3' }
+    let(:new_field_title)            { 'Custom text - updated' }
+    let(:new_field_type)             { 'text' }
+    let(:new_field_description)      { 'A custom text field - updated' }
+    let(:new_field_is_required)      { '1' }
+    let(:new_field_personalized_tag) { 'perstag' }
+    let(:new_field_default_value)    { 'Default value - updated' }
+    let(:new_field_visible)          { '1' }
+    let(:new_field_ordering_number)  { '3' }
     let(:update_params) do
       {
-        title: field_title,
-        type: field_type,
-        descript: field_description,
-        isrequired: field_is_required,
-        # perstag: field_personalized_tag,
-        defval: field_default_value,
-        visible: field_visible,
-        ordernum: field_ordering_number
+        title: new_field_title,
+        type: new_field_type,
+        descript: new_field_description,
+        isrequired: new_field_is_required,
+        # perstag: new_field_personalized_tag,
+        defval: new_field_default_value,
+        visible: new_field_visible,
+        ordernum: new_field_ordering_number
       }
     end
 
-    let(:expected_field_response) do
+    let(:expected_field_update_response) do
       update_params
     end
 
     it 'returns a field hash' do
-      expect(response).to include_json(field: expected_field_response)
+      expect(response).to include_json(field: expected_field_update_response)
     end
   end
 
